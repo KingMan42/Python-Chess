4843d7274a4f490a64c7610e04def3998f45a106
KingMan42 <ryanjking42@gmail.com>
2017-08-08 15:23:57 -0400

    Delete Test grounds.py

--- Test grounds.py
+++
@@ -1,8 +0,0 @@
-is_whites_turn = True
-x = 0
-for is_whites_turn in range (0,5):
-    print(is_whites_turn)
-    is_whites_turn = not is_whites_turn
-while x < 5:
-    print(is_whites_turn)
-    is_whites_turn = not is_whites_turn
